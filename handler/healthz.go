e256f6a0956456ec7234034e3d7c8840e372265a
Taiki Yoshikawa <yoshikawataiki@gmail.com>
2023-12-28 15:54:23 +0900

    update router and healthz

--- handler/healthz.go
+++ handler/healthz.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
@@ -16,5 +18,12 @@ func NewHealthzHandler() *HealthzHandler {
 
 // ServeHTTP implements http.Handler interface.
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_ = &model.HealthzResponse{}
+	response := &model.HealthzResponse{
+		Message: "OK",
+	}
+
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.Println("HealthzHandler: failed to write response, err =", err)
+	}
 }
--- handler/router/router.go
+++ handler/router/router.go
@@ -3,10 +3,17 @@ package router
 import (
 	"database/sql"
 	"net/http"
+
+	"github.com/TechBowl-japan/go-stations/handler"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	// register routes
 	mux := http.NewServeMux()
+	// todoService := service.NewTODOService(todoDB)
+	// todoHandler := handler.NewTODOHandler(todoService)
+	healthzHandler := handler.NewHealthzHandler()
+	mux.Handle("/healthz", healthzHandler)
+
 	return mux
 }
